5f7cad81d5625205dae2494ffbf7f347dd414162
aliS565 <156430722+aliS565@users.noreply.github.com>
2025-07-29 08:08:19 +0300

    Update ScriptModule.js

--- ScriptModule.js
+++ ScriptModule.js
@@ -1,5 +1,4 @@
 
-
   // ✅ استيراد مكتبات Firebase (نفس النسخة)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
